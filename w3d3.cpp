d9dcaba3474a5f2c4ed35a28ecff0ccd5be4dcca
Harvey <harveykwong@outlook.com>
2026-07-03 14:29:23 -0400

    added word pattern

--- w3d3.cpp
+++ w3d3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
@@ -16,9 +17,7 @@ bool isIsomorphic(string s, string t)
         if (letterMap1.find(s[i]) != letterMap1.end())
         {
             if (letterMap1[s[i]] != t[i])
-            {
                 return false;
-            }
         }
         else
         {
@@ -28,9 +27,7 @@ bool isIsomorphic(string s, string t)
         if (letterMap2.find(t[i]) != letterMap1.end())
         {
             if (letterMap2[t[i]] != s[i])
-            {
                 return false;
-            }
         }
         else
         {
@@ -41,10 +38,57 @@ bool isIsomorphic(string s, string t)
     return true;
 }
 
+// Remember to hand out of bounds
+bool wordPattern(string pattern, string s)
+{
+    unordered_map<char, string> letterMap1{};
+    unordered_map<string, char> letterMap2{};
+
+    istringstream iss(s);
+    string currentWord;
+
+    // Extracting words from the string
+    int index{0};
+    while (iss >> currentWord)
+    {
+        char c = pattern[index];
+
+        // Char to String Mapping
+        // If we do find the char in the bank
+        if (letterMap1.find(c) != letterMap1.end())
+        {
+            // Validate the corresponding word with the value from the bank
+            if (currentWord != letterMap1[c])
+                return false;
+        }
+        else
+        {
+            // Brand new char, we add mapping
+            letterMap1[c] = currentWord;
+        }
+
+        // String to Char mapping
+        if (letterMap2.find(currentWord) != letterMap2.end())
+        {
+            if (c != letterMap2[currentWord])
+                return false;
+        }
+        else
+        {
+            letterMap2[currentWord] = c;
+        }
+        ++index;
+    }
+    if (index != pattern.size())
+        return false;
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
-    string test1{"paper"};
-    string test2{"title"};
+    string test1{"abba"};
+    string test2{"dog cat cat dog"};
 
-    cout << isIsomorphic(test1, test2);
+    cout << wordPattern(test1, test2);
 }
