544b6e8432e357eb51294d3cf72e30ee8737fe50
Harvey <harveykwong@outlook.com>
2026-07-07 14:25:27 -0400

    Added group anagrams

--- w4d1.cpp
+++ w4d1.cpp
@@ -2,8 +2,31 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
+
+// This function is destructive to the input argument
+vector<vector<string>> groupAnagrams(vector<string> &strs)
+{
+    // The key of the map will be the sorted version of the word being tested
+    unordered_map<string, vector<string>> wordMap{};
+    vector<vector<string>> returnList{};
+
+    for (string s : strs)
+    {
+        string original = s;
+        sort(s.begin(), s.end());
+
+        wordMap[s].push_back(original);
+    }
+
+    for (const auto &[key, value] : wordMap)
+    {
+        returnList.push_back(value);
+    }
+    return returnList;
+}
 
 bool isAnagram(string s, string t)
 {
@@ -43,8 +66,20 @@ bool isAnagram(string s, string t)
 
 int main(int argc, char *argv[])
 {
-    string testS{"anagraxm"};
-    string testT{"nagarzam"};
+    // string testS{"anagraxm"};
+    // string testT{"nagarzam"};
 
-    cout << isAnagram(testS, testT);
+    // cout << isAnagram(testS, testT);
+
+    vector<string> test{"eat", "tea", "tan", "ate", "nat", "bat"};
+    vector<vector<string>> result = groupAnagrams(test);
+
+    for (const vector<string> s : result)
+    {
+        for (const string str : s)
+        {
+            cout << str << ", ";
+        }
+        cout << endl;
+    }
 }
