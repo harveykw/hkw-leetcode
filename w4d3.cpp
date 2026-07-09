6b8c7cab9bb3c7702f32ecc4533be7ab87363e74
Harvey <harveykwong@outlook.com>
2026-07-09 14:39:50 -0400

    added minimum subarray length question

--- w4d3.cpp
+++ w4d3.cpp
@@ -5,6 +5,57 @@
 
 using namespace std;
 
+int minSubArrayLen(int target, vector<int> &nums)
+{
+
+    int minimalLength{0};
+    bool solExists{false};
+
+    int currentWindowSum{0};
+    vector<int>::iterator it1 = nums.begin();
+    vector<int>::iterator it2 = nums.begin();
+
+    for (int i{0}; i < nums.size(); i++)
+    {
+        currentWindowSum += *it1;
+        it1++;
+
+        if (currentWindowSum >= target)
+        {
+            // Reduce left edge
+            bool reduce{true};
+            while (reduce)
+            {
+                if (currentWindowSum - *it2 >= target)
+                {
+                    currentWindowSum -= *it2;
+                    it2++;
+                }
+                else
+                {
+                    reduce = false;
+                }
+            }
+
+            // Updates minimum length
+            int windowSize = distance(it2, it1);
+            if (solExists)
+            {
+                if (windowSize < minimalLength)
+                {
+                    minimalLength = windowSize;
+                }
+            }
+            else
+            {
+                minimalLength = windowSize;
+                solExists = true;
+            }
+        }
+    }
+    return minimalLength;
+}
+
 bool containsNearbyDuplicate(vector<int> &nums, int k)
 {
     unordered_map<int, int> numBank{};
@@ -32,6 +83,6 @@ bool containsNearbyDuplicate(vector<int> &nums, int k)
 
 int main(int argc, char *argv[])
 {
-    vector<int> test{1, 0, 1, 1};
-    cout << containsNearbyDuplicate(test, 0);
+    vector<int> test{1, 1, 1, 1, 1, 1, 1, 1};
+    cout << minSubArrayLen(11, test);
 }
