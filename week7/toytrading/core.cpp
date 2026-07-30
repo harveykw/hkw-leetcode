4177d2001862552f5eac3c02829b5f9c9009cfb7
Harvey <harveykwong@outlook.com>
2026-07-29 21:35:22 -0400

    WIP. Blocked out major functions

--- week7/toytrading/core.cpp
+++ week7/toytrading/core.cpp
@@ -1,6 +1,7 @@
 
 #include "headers.hpp"
 #include <iostream>
+#include <cmath>
 
 /*
 Price functions
@@ -12,7 +13,17 @@ int Prices::getPrice(const std::string &stockName)
 {
     auto it = this->priceMap.find(stockName);
     if (it != this->priceMap.end())
-        return this->priceMap[stockName];
+        return this->priceMap[stockName].first;
+
+    return -1;
+}
+
+// Returns -1 if updateNum does not exist
+int Prices::getPrice(const std::string &stockName)
+{
+    auto it = this->priceMap.find(stockName);
+    if (it != this->priceMap.end())
+        return this->priceMap[stockName].second;
 
     return -1;
 }
@@ -32,6 +43,63 @@ std::vector<std::string> *Prices::getInvolvedPairs(const std::string &stockName)
     auto it = this->involvedPairs.find(stockName);
     if (it != this->involvedPairs.end())
         return &(this->involvedPairs[stockName]);
+
+    return nullptr;
+}
+
+// Note, this overwrites and has default behavior
+void Prices::updateRawPrice(
+    const std::string &product,
+    std::int64_t newPrice,
+    std::int64_t updateNumber)
+{
+    this->priceMap[product] = {newPrice, updateNumber};
+}
+
+/*
+Price monitor functions
+
+*/
+
+std::vector<Alert> PriceMonitor::updatePrice(
+    const std::string &product,
+    std::int64_t newPrice,
+    std::int64_t updateNumber)
+{
+    std::vector<Alert> alerts{};
+
+    Prices *priceObj = this->priceObj.get();
+    int curUpdate{priceObj->getUpdateNum(product)};
+
+    // Consider case where not init or update number supercedes current
+    if (curUpdate < updateNumber)
+        priceObj->updateRawPrice(product, newPrice, updateNumber);
+
+    // Now scan through paired relationships and send an alert if needed
+    std::vector<std::string> *involvedPairs = priceObj->getInvolvedPairs(product);
+    if (involvedPairs == nullptr)
+        return {}; // No paired relationships involving this specific stock
+
+    for (std::string pairId : *involvedPairs) // Looping though all the pairId that need tolerance checking
+    {
+        StockPair *stock = priceObj->getPair(pairId);
+        int price1{priceObj->getPrice(stock->firstProduct)};
+        int price2{priceObj->getPrice(stock->secondProduct)};
+
+        if (abs(price1 - price2) > stock->maximumAllowedDifference)
+        { // Distance exceeds limit - Send alert! (by adding to alerts vector)
+        }
+    }
+
+    return alerts;
+}
+
+void PriceMonitor::registerPair(
+    const std::string &pairId,
+    const std::string &firstProduct,
+    const std::string &secondProduct,
+    std::int64_t maximumAllowedDifference)
+{
 }
 
 int main(int argc, char *argv[])
--- week7/toytrading/headers.hpp
+++ week7/toytrading/headers.hpp
@@ -4,35 +4,6 @@
 #include <unordered_map>
 #include <memory>
 
-struct Alert
-{
-    std::string pairId;
-    std::string firstProduct;
-    std::string secondProduct;
-    std::int64_t difference;
-};
-
-class PriceMonitor
-{
-public:
-    void registerPair(
-        const std::string &pairId,
-        const std::string &firstProduct,
-        const std::string &secondProduct,
-        std::int64_t maximumAllowedDifference);
-
-    std::vector<Alert> updatePrice(
-        const std::string &product,
-        std::int64_t newPrice,
-        std::int64_t updateNumber);
-
-    // Constructor
-    PriceMonitor(std::unique_ptr<Prices>);
-
-private: // Add any private data members or helper functions you need.
-    std::unique_ptr<Prices> priceObj;
-};
-
 /*
 This is the class that will hold the information regarding a specific pair of stock relationships
 
@@ -57,19 +28,58 @@ public:
     // Function to get price of stock by stock name
     int getPrice(const std::string &stockName);
 
+    // Function to get updateNum of stock by stock name
+    int getUpdateNum(const std::string &stockName);
+
     // Function to get StockPair ptr from pairId
     StockPair *getPair(const std::string &pairId);
 
     // Function to get vector of pairs names involved with a specific stock name
-    std::vector<std::string> getInvolvedPairs(const std::string &stockName);
+    std::vector<std::string> *getInvolvedPairs(const std::string &stockName);
+
+    void updateRawPrice(
+        const std::string &product,
+        std::int64_t newPrice,
+        std::int64_t updateNumber);
 
 private:
     // Holds the raw prices (stock name -> price)
-    std::unordered_map<std::string, int> priceMap;
+    std::unordered_map<std::string, std::pair<int, int>> priceMap;
 
     // Holds the pairs (pairId -> StockPair obj ptr)
     std::unordered_map<std::string, std::unique_ptr<StockPair>> pairMap;
 
     // Tracks which pairs are involved with a specific pairId
     std::unordered_map<std::string, std::vector<std::string>> involvedPairs;
+};
+
+class PriceMonitor
+{
+public:
+    void registerPair(
+        const std::string &pairId,
+        const std::string &firstProduct,
+        const std::string &secondProduct,
+        std::int64_t maximumAllowedDifference);
+
+    std::vector<Alert> updatePrice(
+        const std::string &product,
+        std::int64_t newPrice,
+        std::int64_t updateNumber);
+
+    // Constructor
+    PriceMonitor(std::unique_ptr<Prices> prices) : priceObj(std::move(prices)) {}
+
+private: // Add any private data members or helper functions you need.
+    std::unique_ptr<Prices> priceObj;
+};
+
+struct Alert
+{
+    std::string pairId;
+    std::string firstProduct;
+    std::string secondProduct;
+    std::int64_t difference;
+
+    Alert(std::string pairId, std::string firstProduct, std::string secondProduct, std::int64_t difference) : pairId(pairId), firstProduct(firstProduct), secondProduct(secondProduct), difference(difference) {}
 };
