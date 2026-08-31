27fae3cdd64c863832c679930ff7b4366a1d8f8e
Harvey <harveykwong@outlook.com>
2026-08-31 15:20:24 -0400

    Added lowest common ancestor of binary tree

--- week11/w11d1.cpp
+++ week11/w11d1.cpp
@@ -36,4 +36,32 @@ public:
     {
         return (recursiveSum(root, 0));
     }
+
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+
+        if (root == nullptr)
+            return nullptr;
+
+        if (root == p || root == q)
+        {
+            return root;
+        }
+        // No Children
+        if (root->left == nullptr && root->right == nullptr)
+            return nullptr;
+
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right)
+            return root;
+
+        if (left)
+            return left;
+        if (right)
+            return right;
+
+        return nullptr;
+    }
 };
