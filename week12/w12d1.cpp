5d534a30ddbe4718b29f146b8d01bf1db5b7bf68
Harvey <harveykwong@outlook.com>
2026-09-07 13:54:47 -0400

    added merge intervals

--- week12/w12d1.cpp
+++ week12/w12d1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <algorithm>
 
 using std::vector, std::string, std::pair;
 
@@ -45,6 +46,41 @@ public:
             }
 
             returnVector.push_back(std::to_string(currentPair.first) + "->" + std::to_string(currentPair.second));
+        }
+
+        return returnVector;
+    }
+
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        vector<vector<int>> returnVector{};
+        if (intervals.empty())
+            return returnVector;
+
+        // Sort by first element, then tie break with latter elements as default vector sorting behavior
+        std::sort(intervals.begin(), intervals.end());
+
+        for (const auto &interval : intervals)
+        {
+            if (returnVector.empty())
+            {
+                returnVector.push_back(interval);
+                continue;
+            }
+
+            // case one - fully contained
+            if (interval.at(0) >= returnVector.back().at(0) && interval.at(1) <= returnVector.back().at(1))
+                continue;
+
+            // case two - partial containment
+            if (interval.at(0) >= returnVector.back().at(0) && interval.at(0) <= returnVector.back().at(1))
+            {
+                returnVector.back().at(1) = interval.at(1);
+                continue;
+            }
+
+            // case three - disjoint interval
+            returnVector.push_back(interval);
         }
 
         return returnVector;
