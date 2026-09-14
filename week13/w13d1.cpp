e91f17690508d9f279bddaf822c6b2b2e0df6b93
Harvey <harveykwong@outlook.com>
2026-09-14 12:17:13 -0400

    merge two sorted linked lists

--- week13/w13d1.cpp
+++ week13/w13d1.cpp
@@ -71,6 +71,59 @@ public:
 
         return head;
     }
+
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+
+        if (!(list1 || list2))
+            return nullptr;
+
+        vector<ListNode *> accumulatedNodes{};
+
+        while (list1 != nullptr || list2 != nullptr)
+        {
+
+            // If at least one is missing
+            if (!(list1 && list2))
+            {
+                if (list1 != nullptr)
+                {
+                    accumulatedNodes.push_back(list1);
+                    list1 = list1->next;
+                }
+
+                if (list2 != nullptr)
+                {
+                    accumulatedNodes.push_back(list2);
+                    list2 = list2->next;
+                }
+
+                continue;
+            }
+
+            // If both are present
+            if (list1->val < list2->val)
+            {
+                accumulatedNodes.push_back(list1);
+                list1 = list1->next;
+            }
+            else
+            {
+                accumulatedNodes.push_back(list2);
+                list2 = list2->next;
+            }
+        }
+
+        for (int i{0}; i < accumulatedNodes.size(); ++i)
+        {
+            if (i + 1 < accumulatedNodes.size())
+            {
+                accumulatedNodes.at(i)->next = accumulatedNodes.at(i + 1);
+            }
+        }
+
+        return accumulatedNodes.front();
+    }
 };
 
 int main(int argc, char *argv[])
